b38d0b9937444a1e1f65b2e5f5cc78d19006d55d
moraleseoscar <oestrada.3@gmail.com>
2023-08-03 04:06:49 -0600

    Fixing bugs in Hamming Receiver

--- Receiver.py
+++ Receiver.py
@@ -1,5 +1,4 @@
-print('Enter the hamming code received')
-d=input()
+d=input('Enter the hamming code received: ')
 data=list(d)
 data.reverse()
 c,ch,j,r,error,h,parity_list,h_copy=0,0,0,0,0,[],[],[]
@@ -32,19 +31,24 @@ parity_list.reverse()
 error=sum(int(parity_list) * (2 ** i) for i, parity_list in enumerate(parity_list[::-1]))
 
 if((error)==0):
-    print('There is no error in the hamming code received')
-
+    print('No hay error en la trama recibida')
+    data.reverse()
+    decoded_message = "".join(str(bit) for i, bit in enumerate(data) if i not in [2**k - 1 for k in range(len(data))])
+    print("Trama recibida: ", decoded_message)
 elif((error)>=len(h_copy)):
-    print('Error cannot be detected')
-
+    print('No hay error para detectar')
+    data.reverse()
+    decoded_message = "".join(str(bit) for i, bit in enumerate(data) if i not in [2**k - 1 for k in range(len(data))])
+    print("Trama recibida: ", decoded_message)
 else:
-    print('Error is in',error,'bit')
-
+    print('Hay error en el bit ',error)
+    h_copy.reverse()
     if(h_copy[error-1]=='0'):
         h_copy[error-1]='1'
 
     elif(h_copy[error-1]=='1'):
         h_copy[error-1]='0'
-        print('After correction hamming code is:- ')
-    h_copy.reverse()
-    print(int(''.join(map(str, h_copy))))
+    print('Despues de correccion por Hamming:- ',int(''.join(map(str, h_copy))))
+    decoded_message = "".join(str(bit) for i, bit in enumerate(h_copy) if i not in [2**k - 1 for k in range(len(h_copy))])
+    print("Trama recibida: ", decoded_message)
+
