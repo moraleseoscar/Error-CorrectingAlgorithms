882b89bedd41f0c93f5f2fd3d2f98452c98deb9e
Gabriel Alejandro Vicente Lorenzo <60375344+GabrielVicente-GT@users.noreply.github.com>
2023-08-10 03:30:56 -0600

    Add files via upload

---
+++ ReceiverLab2.py
@@ -0,0 +1,151 @@
+
+import socket
+
+#Variables para conexión y recibimiento de mensajes
+HOST = "127.0.0.1" # IP, capa de Red. 127.0.0.1 es localhost
+PORT = 65432 # Puerto, capa de Transporte
+
+
+#-------------------------------------Decodificador de Binario------------------------------------------
+
+def binary_to_words(binary_string):
+    # Divide la cadena binaria en grupos de 8 bits (un byte)
+    bytes_list = [binary_string[i:i+8] for i in range(0, len(binary_string), 8)]
+    
+    # Convierte cada byte en su equivalente decimal y luego en un caracter
+    characters = [chr(int(byte, 2)) for byte in bytes_list]
+    
+    # Une los caracteres para formar las palabras
+    words = ''.join(characters)
+    
+    return words
+
+#-------------------------------------CRC Receptor------------------------------------------
+def xor_binary_strings(str1, str2):
+    if len(str1) != len(str2):
+        print("Las cadenas deben tener la misma longitud para realizar la operación XOR.")
+    result = ""
+    for bit1, bit2 in zip(str1, str2):
+        xor_result = int(bit1) ^ int(bit2)
+        result += str(xor_result)
+    return list(result)
+
+def crc_receptor(trama):
+    trama_lista = list(trama)
+    crc32   = "100000100110000010001110110110111"
+
+    # Inicia logica para CRC 32
+    A = trama_lista[:33]
+    trama_lista = trama_lista[33:]
+    resultado = xor_binary_strings(''.join(A),crc32)
+
+    while len(trama_lista) > 0:
+        if resultado[0] == '0':
+            resultado.pop(0)
+            agregar = trama_lista.pop(0)
+            resultado.append(agregar)
+        if resultado[0] == '1':
+            resultado = xor_binary_strings(resultado,crc32)
+    
+    respuesta = True
+
+    for x in resultado:
+        if x != '0':
+            respuesta = False
+    
+    mensaje_original = list(trama)
+    
+    if respuesta:
+        for x in range(0,32):
+            mensaje_original.pop()
+    else:
+        mensaje_original = 'Se encontraron problemas'
+    
+    return mensaje_original
+
+#-------------------------------------Hamming Receptor------------------------------------------
+
+def hamming_receptor(d):
+    data=list(d)
+    data.reverse()
+    c,ch,j,r,error,h,parity_list,h_copy=0,0,0,0,0,[],[],[]
+
+    for k in range(0,len(data)):
+        p=(2**c)
+        h.append(int(data[k]))
+        h_copy.append(data[k])
+        if(p==(k+1)):
+            c=c+1
+            
+    for parity in range(0,(len(h))):
+        ph=(2**ch)
+        if(ph==(parity+1)):
+
+            startIndex=ph-1
+            i=startIndex
+            toXor=[]
+
+            while(i<len(h)):
+                block=h[i:i+ph]
+                toXor.extend(block)
+                i+=2*ph
+
+            for z in range(1,len(toXor)):
+                h[startIndex]=h[startIndex]^toXor[z]
+            parity_list.append(h[parity])
+            ch+=1
+    parity_list.reverse()
+    error=sum(int(parity_list) * (2 ** i) for i, parity_list in enumerate(parity_list[::-1]))
+
+    if((error)==0):
+        print('No hay error en la trama recibida')
+        data.reverse()
+        decoded_message = "".join(str(bit) for i, bit in enumerate(data) if i not in [2**k - 1 for k in range(len(data))])
+    elif((error)>=len(h_copy)):
+        print('No hay error para detectar')
+        data.reverse()
+        decoded_message = "".join(str(bit) for i, bit in enumerate(data) if i not in [2**k - 1 for k in range(len(data))])
+    else:
+        print('Hay error en el bit ',error)
+        h_copy.reverse()
+        if(h_copy[error-1]=='0'):
+            h_copy[error-1]='1'
+
+        elif(h_copy[error-1]=='1'):
+            h_copy[error-1]='0'
+        print('Despues de correccion por Hamming:- ',int(''.join(map(str, h_copy))))
+        decoded_message = "".join(str(bit) for i, bit in enumerate(h_copy) if i not in [2**k - 1 for k in range(len(h_copy))])
+    return decoded_message
+
+#-------------------------------------Conexión Socket------------------------------------------
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+
+    conn, addr = s.accept()
+    with conn:
+        while True:
+            try:
+                print(f"Conexion Entrante del proceso {addr}")
+                full_data = conn.recv(1024).decode("utf-8")
+                selections_and_messages = full_data.split("\n")
+                # print(selections_and_messages)
+                selAlgorithm, message,_ = selections_and_messages
+                message = message.replace('\r','')
+                
+                print("\nTrama recibida: ", message) 
+                if(selAlgorithm == "1"):
+                    print("Algoritmo seleccionado: Hamming-Code 7,4") 
+                    # Mandamos trama a Hamming
+                    print("\nReceptor por Hamming:\n----------------------")
+                    receptor = hamming_receptor(message)
+                    print("Trama recibida:", receptor)
+                else:
+                    print("Algoritmo seleccionado: CRC-32") 
+                    # Mandamos trama a CRC
+                    print("\nReceptor por CRC32:\n----------------------")
+                    receptor = ''.join(crc_receptor(message))
+                    print(receptor, "\n")
+                print("Letra recibida:", binary_to_words(receptor), "\n")
+            except:
+                break
---
+++ SenderLab2.java
@@ -0,0 +1,305 @@
+// package Redes.Laboratorio 2.Parte 2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Set;
+
+public class SenderLab2 {
+
+    public static void main(String[] args) {
+        String hostname = "localhost";
+        int port = 65432;
+        Scanner scanner = new Scanner(System.in);
+        Set<String> palabrasGeneradas = generarPalabras();
+        try {
+            Socket socket = new Socket(hostname, port);
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+            BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
+
+            System.out.println("Ingrese un mensaje para enviar (escriba 'exit' para salir):");
+            // ! For x in range 10k
+                
+            for (String palabra : palabrasGeneradas) {
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                    // Aca va toda la lógica con "palabra"
+                System.out.print("Ingresa un texto: ");
+                // String inputText = scanner.next();
+                String inputText = palabra;
+                // scanner.next();
+                // System.out.println("\nOpciones:");
+                // Conversión de texto a binario.
+                String binaryResult = textToBinary(inputText);
+
+                System.out.println("\nOpciones:");
+                System.out.println("1. Hamming");
+                System.out.println("2. CRC-32");
+                System.out.print("Selecciona una opción: ");
+                // int choice = scanner.nextInt();
+                // int choice = 1;
+                int choice = 2;
+                // scanner.next();
+                System.out.print("\n");
+                switch (choice) {
+                    case 1:
+                        binaryResult = hamming(binaryResult);
+                        // Aquí podrías agregar la lógica para la codificación Hamming
+                        System.out.println("Opción Hamming seleccionada");
+                        break;
+                    case 2:
+                        binaryResult = crc_emisor(binaryResult);
+                        // Calcula el valor CRC-32 y muestra el resultado
+                        System.out.println("Opción Hamming seleccionada");
+                        break;
+                    case 3:
+                        // Calcula el valor CRC-32 y muestra el resultado
+                        System.out.println("Gracias por usar signal 2!");
+                        break;
+                    default:
+                        System.out.println("Opción no válida.");
+                }
+                System.out.println("Texto: " + inputText);
+                System.out.println("En binario: " + binaryResult);
+                
+                // String message = consoleReader.readLine();
+                String resultado = String.valueOf(choice)+"\n"+binaryResult;
+                System.out.println(resultado);
+                if (String.valueOf(choice).equalsIgnoreCase("3")) {
+                    break;
+                }
+                out.println(resultado);
+            }
+            
+            socket.close();
+            consoleReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        scanner.close();
+    }
+
+    public static Set<String> generarPalabras() {
+        Set<String> palabrasGeneradas = new HashSet<>();
+        Random random = new Random();
+        char[] letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+
+        while (palabrasGeneradas.size() < 100) { // Puedes ajustar la cantidad de palabras a generar
+            StringBuilder palabra = new StringBuilder();
+            int longitud = random.nextInt(4) + 1;
+
+            for (int i = 0; i < longitud; i++) {
+                char letra = letras[random.nextInt(letras.length)];
+                palabra.append(letra);
+            }
+
+            palabrasGeneradas.add(palabra.toString());
+        }
+
+        return palabrasGeneradas;
+    }
+
+    
+    public static String textToBinary(String text) {
+        StringBuilder binaryStringBuilder = new StringBuilder();
+        for (char c : text.toCharArray()) {
+            String binaryString = Integer.toBinaryString(c);
+            binaryStringBuilder.append(String.format("%8s", binaryString).replace(' ', '0'));
+        }
+        return binaryStringBuilder.toString();
+    }
+    public static List<String> xorLists(List<String> list1, List<String> list2) {
+        if (list1.size() != list2.size()) {
+            throw new IllegalArgumentException("Las listas deben tener la misma longitud");
+        }
+        
+        List<String> result = new ArrayList<>();
+        for (int i = 0; i < list1.size(); i++) {
+            String bit1 = list1.get(i);
+            String bit2 = list2.get(i);
+            
+            if (bit1.equals("0") && bit2.equals("0")) {
+                result.add("0");
+            } else if (bit1.equals("1") && bit2.equals("0")) {
+                result.add("1");
+            } else if (bit1.equals("0") && bit2.equals("1")) {
+                result.add("1");
+            } else if (bit1.equals("1") && bit2.equals("1")) {
+                result.add("0");
+            } else {
+                throw new IllegalArgumentException("Las listas deben contener solo caracteres '0' y '1'");
+            }
+        }
+        
+        return result;
+    }
+    public static String concatenateStrings(List<String> strings) {
+        StringBuilder stringBuilder = new StringBuilder();
+        
+        for (String str : strings) {
+            stringBuilder.append(str);
+        }
+        
+        return stringBuilder.toString();
+    }
+    public static String crc_emisor(String trama) {
+
+        // El input se convierte a lista
+        List<String> trama_lista = new ArrayList<>();
+        for (char c : trama.toCharArray()) {
+            trama_lista.add(new String(new char[]{c}));
+        }
+
+        // Polinomio que se utilizara para las operaciones
+        List<String> crc32 = new ArrayList<>();
+        char[] temp_crc32= "100000100110000010001110110110111".toCharArray();
+        for (char c : temp_crc32) {
+            crc32.add(new String(new char[]{c}));
+        }
+        // Se agregan la cantidad de 0 a la trama
+
+        for (int i = 0; i < (crc32.size()-1); i++) {
+            trama_lista.add("0");
+        }
+
+        
+        
+        // ! Inicia procedimiento para CRC 32
+        
+        
+        // * Se declara A
+
+        List<String> A = new ArrayList<>();
+        int limit = Math.min(33, trama_lista.size());
+        for (int i = 0; i < limit; i++) {
+            A.add(trama_lista.get(i));
+        }
+        
+        // * Se actualiza trama 
+        
+        if (trama_lista.size() >= 33) {
+            trama_lista.subList(0, 33).clear();
+        } else {
+            System.out.println("La lista no tiene suficientes elementos para eliminar 33.");
+        }
+        
+        // * Se realiza la operacion entre A y crc32
+
+        List<String> resultado = xorLists(A, crc32);
+        
+        // System.out.println("Trama inicial" + trama_lista);
+        // System.out.println("Resultado inicial" + resultado);
+
+
+        // * Inicia Ciclo
+
+        while (trama_lista.size() > 0) {
+            if(resultado.get(0).equals("0")){
+                resultado.remove(0);
+                String agregar = trama_lista.remove(0);
+                resultado.add(agregar);
+                // System.out.println("Trama C0 " + trama_lista);
+                // System.out.println("Resultado C0" + resultado);
+            }
+            if(resultado.get(0).equals("1")){
+                resultado = xorLists(resultado, crc32);
+                // System.out.println("Trama C1" + trama_lista);
+                // System.out.println("Resultado C1 " + resultado);
+            }
+
+        }
+        // System.out.println("Trama 0" + trama_lista);
+        // System.out.println("Resultado 0" + resultado);
+
+        List<String> respuesta = new ArrayList<>();
+        for (char c : trama.toCharArray()) {
+            respuesta.add(new String(new char[]{c}));
+        }
+
+        int startIndex = Math.max(0, resultado.size() - 32); // Asegurarse de no exceder el tamaño de la lista
+        for (int i = startIndex; i < resultado.size(); i++) {
+            respuesta.add(resultado.get(i));
+        }
+        String resultadoConcatenado = concatenateStrings(respuesta);
+        // System.out.println("asdfaf " + trama_lista.size());
+        // System.out.println("asdfaf " + A);
+        // System.out.println("asdfaf " + A.size());
+        // System.out.println("asdfaf " + crc32);
+        // System.out.println("asdfaf " + crc32.size());
+
+        return resultadoConcatenado;
+    }
+    public static String hamming(String trama) {
+
+        // Scanner scanner = new Scanner(System.in);
+        // System.out.print("Enter the data bits: ");
+        String inputData = trama;
+
+        int len = inputData.length();
+        char[] data = new char[len];
+        inputData.getChars(0, len, data, 0);
+        int r = 0;
+        int c = 0;
+        int ch = 0;
+        int j = 0;
+        int[] h = new int[100];
+
+        while ((len + r + 1) > Math.pow(2, r)) {
+            r++;
+        }
+
+        for (int i = 0; i < (r + len); i++) {
+            int p = (int) Math.pow(2, c);
+            if (p == (i + 1)) {
+                h[i] = 0;
+                c++;
+            } else {
+                h[i] = data[j] - '0';
+                j++;
+            }
+        }
+
+        for (int parity = 0; parity < (r + len); parity++) {
+            int ph = (int) Math.pow(2, ch);
+            if (ph == (parity + 1)) {
+                int startIndex = ph - 1;
+                int i = startIndex;
+                int[] toXor = new int[100];
+                int toXorIdx = 0;
+
+                while (i < (r + len)) {
+                    for (int idx = i; idx < i + ph; idx++) {
+                        if (idx < (r + len)) {
+                            toXor[toXorIdx] = h[idx];
+                            toXorIdx++;
+                        }
+                    }
+                    i += 2 * ph;
+                }
+
+                for (int z = 1; z < toXorIdx; z++) {
+                    h[startIndex] = h[startIndex] ^ toXor[z];
+                }
+                ch++;
+            }
+        }
+        List<String> A = new ArrayList<>();
+        for (int i = 0; i < (r + len); i++) {
+            A.add(String.valueOf(h[i]));
+        }
+        String hString = concatenateStrings(A);
+        return hString;
+    }
+    
+}
